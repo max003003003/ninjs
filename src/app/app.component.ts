3998f980abf9987c884447ac8a9f4161126b7916
vorabhol chuaybhan <max003003003@gmail.com>
2016-11-04 23:20:33 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <ul>
+    <li *ngFor="let item of items | async">
+       {{  item.name }}
+    </li>
+  </ul>
+  `,
 })
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any>;
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/hero');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyCO7AxFx0cb_FQGCyfVtwGUIjq3gJ6WVpc",
+    authDomain: "contact-3281f.firebaseapp.com",
+    databaseURL: "https://contact-3281f.firebaseio.com",
+    storageBucket: "contact-3281f.appspot.com",
+    messagingSenderId: "742032670073"
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
